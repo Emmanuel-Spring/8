2d25eddf6e6719230add93589c3e71cbc5d90563
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-14 15:59:01 -0300

    Java master

--- src/Semana02/Teoria/PaquetesPackages.java
+++ src/Semana02/Teoria/PaquetesPackages.java
@@ -2,9 +2,12 @@ package Semana02.Teoria;
 
 import java.util.Scanner;
 
-/** Java Packages
+/** Java Packages  --  Scanner de import java.util.Scanner;
+ *  Clase Scanner: Introducimos por consola el nombre, apellidos y rut, y nos devuelve los datos
+ *  ingresados de forma ordenada.
  *
- * @author Emmanuel Nieto Muñoz   ||  Ignacio Mena Godoy
+ * @author Emmanuel Nieto Muñoz
+ * @author Ignacio Mena Godoy
  * @version: 13/10/2021/1.0
  * @see <a href = "https://www.w3schools.com/java/java_packages.asp" /> Java Packages </a>
  */
@@ -19,9 +22,18 @@ public class PaquetesPackages {
         System.out.println("Ingrese su apellidos:   ");
         String apellidoUsuario = myObj1.nextLine();
 
+        Scanner myObj2 = new Scanner(System.in);
+        System.out.println("Ingrese su rut:   ");
+        String rutUsuario = myObj2.nextLine();
 
+
+        System.out.println("*****************************");
+        System.out.println("*****************************\n\n");
         System.out.println("Su nombre de usuario es:   "  + nombreUsuario);
         System.out.println("Su apellidos de usuario es:   " + apellidoUsuario);
+        System.out.println("El rut del usuario es:   " + rutUsuario);
+        System.out.println("\n\n*****************************");
+        System.out.println("*****************************");
 
 
     }
