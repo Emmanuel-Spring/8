281df19ddffaefb80075ab7f860f635a7f6751ca
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-30 15:47:02 -0300

    Project Config

---
+++ src/Semana11/Tarea/Main.java
@@ -0,0 +1,11 @@
+package Semana10.Tarea;
+
+/** Java Title
+ *
+ * @author Emmanuel Nieto Muñoz
+ * @version: xx/10/2021/1.0
+ * @see <a href = "" />  </a>
+ */
+
+public class Main {
+}
---
+++ src/Semana11/Teoria/Main.java
@@ -0,0 +1,13 @@
+package Semana10.Teoria;
+
+/** Java Title
+ *
+ * @author Emmanuel Nieto Muñoz
+ * @version: xx/10/2021/1.0
+ * @see <a href = "" />  </a>
+ */
+
+public class Main {
+
+
+}
